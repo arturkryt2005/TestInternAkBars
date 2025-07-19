0485624d59864dff4649d4cb940eb05ac91d9cc3
Артур <125348508+arturkryt2005@users.noreply.github.com>
2025-07-19 15:33:56 +0300

    Добавьте файлы проекта.

---
+++ TestInternAkBars/ImageFilter.cs
@@ -0,0 +1,50 @@
+﻿using TestInternAkBars.Models;
+
+namespace TestInternAkBars
+{
+    public static class ImageFilter
+    {
+        public static Pixel[,] ApplyMedianFilter(Pixel[,] image)
+        {
+            int height = image.GetLength(0);
+            int width = image.GetLength(1);
+
+            var result = new Pixel[height, width];
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    result[y, x] = GetMedianPixel(image, x, y, width, height);
+                }
+            }
+
+            return result;
+        }
+
+        private static Pixel GetMedianPixel(Pixel[,] image, int x, int y, int width, int height)
+        {
+            List<Pixel> window = new List<Pixel>();
+
+            for (int j = -1; j <= 1; j++)
+            {
+                for (int i = -1; i <= 1; i++)
+                {
+                    int newX = x + i;
+                    int newY = y + j;
+
+                    if (newX < 0) newX = 0;
+                    if (newX >= width) newX = width - 1;
+                    if (newY < 0) newY = 0;
+                    if (newY >= height) newY = height - 1;
+
+                    window.Add(image[newY, newX]);
+                }
+            }
+
+            window.Sort((p1, p2) => p1.GetLuminance().CompareTo(p2.GetLuminance()));
+
+            return window[4]; 
+        }
+    }
+}
---
+++ TestInternAkBars/Models/Pixel.cs
@@ -0,0 +1,18 @@
+﻿namespace TestInternAkBars.Models
+{
+    public class Pixel
+    {
+        public byte Red { get; set; }
+        public byte Green { get; set; }
+        public byte Blue { get; set; }
+
+        public Pixel(byte r, byte g, byte b)
+        {
+            Red = r;
+            Green = g;
+            Blue = b;
+        }
+
+        public int GetLuminance() => (Red + Green + Blue) / 3;
+    }
+}
